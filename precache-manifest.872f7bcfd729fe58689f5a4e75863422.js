886b5002feb6ae52a7931eda3abe8f7acc5048f0
LeilaEnglund <leilae@kth.se>
2020-04-08 20:30:59 +0200

    Updates

---
+++ precache-manifest.872f7bcfd729fe58689f5a4e75863422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe9653f27d5db182e06781608abf791",
+    "url": "/Beaubi/index.html"
+  },
+  {
+    "revision": "d6a6b962040ad2132ac8",
+    "url": "/Beaubi/static/css/main.363e29ca.chunk.css"
+  },
+  {
+    "revision": "19ba4ceff463b7392f6d",
+    "url": "/Beaubi/static/js/2.10bc15b0.chunk.js"
+  },
+  {
+    "revision": "d6a6b962040ad2132ac8",
+    "url": "/Beaubi/static/js/main.a7300fa9.chunk.js"
+  },
+  {
+    "revision": "7d47c8369d2a9fee2ecb",
+    "url": "/Beaubi/static/js/runtime~main.592a197f.js"
+  }
+]);
